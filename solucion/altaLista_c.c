215500c0256ff9cf683e259070a9cec2448cc9b7
Ivan Arcuschin <iarcuschin@gmail.com>
2015-04-03 19:52:14 -0300

    Arreglo funcion string_iguales que habia cambiado

--- solucion/altaLista_c.c
+++ solucion/altaLista_c.c
@@ -4,8 +4,8 @@
 
 bool string_iguales( char *s1, char *s2 ){
    int i = 0;
-   while( s1[i] != 0 && s2[i] != 0 ) {
-      if( s1[i] == s2[i] )
+   while( s1[i] == s2[i] ) {
+      if( s1[i] == 0 || s2[i] == 0 )
          break;
       i++;
    }
