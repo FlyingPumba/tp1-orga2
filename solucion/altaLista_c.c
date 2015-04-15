13af77579910ccae001122e1f390724cae2cbb3a
Ivan Arcuschin <iarcuschin@gmail.com>
2015-04-15 11:38:27 -0300

    Archivos como los que vienen en el fuente

--- solucion/altaLista_c.c
+++ solucion/altaLista_c.c
@@ -1,10 +1,15 @@
 #include "altaLista.h"
 
+
+
+
+
+
 /** Funciones Auxiliares ya implementadas en C **/
 
 bool string_iguales( char *s1, char *s2 ){
    int i = 0;
-   while( s1[i] == s2[i] ) {
+   while( s1[i] == s2[i] ){
       if( s1[i] == 0 || s2[i] == 0 )
          break;
       i++;
@@ -25,3 +30,4 @@ void insertarAtras( altaLista *l, void *dato ){
     nuevoNodo->anterior = l->ultimo;
     l->ultimo = nuevoNodo;
 }
+
